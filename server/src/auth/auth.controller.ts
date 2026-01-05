8ad0d1afe203aa733427bef052ede634edf047fd
Bully_Thunder <vamosbarca2407@gmail.com>
2026-01-05 11:46:12 +0200

    save

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
-import { Controller,Post,Body } from '@nestjs/common';
-import { AuthService } from './auth.service'
-import { RegisterDto } from './dto/register.dto'
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('register')
-    async register(@Body() dto:RegisterDto){
-        return this.authService.register(dto);
-    }
-    @Post('login')
-    async login(@Body() dto:LoginDto){
-        return this.authService.login(dto)
-    }
+  @Post('register')
+  async register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
+  }
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,45 +1,73 @@
-import { Injectable, BadRequestException,UnauthorizedException} from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma : PrismaService ) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-    async register(dto: RegisterDto){
-        const existingUser = await this.prisma.user.findUnique({
-            where: {
-                email: dto.email
-            },
-        })
-        if(existingUser){
-            throw new BadRequestException('Incorrect email format or user already exists');
-            }  
-        const hashedPassword = await bcrypt.hash(dto.password, 10);
-
-        const user = await this.prisma.user.create({
-        data: {
-            email: dto.email,
-            name: dto.name,
-            passwordHash: hashedPassword
-            }
-        })
-        const {passwordHash, ...userWithoutPassword} = user;
-        return userWithoutPassword;
+  async register(dto: RegisterDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (existingUser) {
+      throw new BadRequestException(
+        'Incorrect email format or user already exists',
+      );
     }
-    async login(dto: LoginDto){
-        const findUser = await this.prisma.user.findUnique({
-            where: {
-                email: dto.email,
-            }
-        })
-        if(findUser === null){
-             throw new UnauthorizedException("false data")
-        }
-        else{
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
 
-        }
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: dto.name,
+        passwordHash: hashedPassword,
+      },
+    });
+    const userWithoutPassword = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    };
+    return userWithoutPassword;
+  }
+  async login(dto: LoginDto) {
+    const findUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (findUser === null) {
+      throw new UnauthorizedException('false data');
     }
+    const truePassword = await bcrypt.compare(
+      dto.password,
+      findUser.passwordHash,
+    );
+    if (!truePassword) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+    const finalUser = {
+      id: findUser.id,
+      email: findUser.email,
+      name: findUser.name,
+    };
+    return finalUser;
+  }
+  private async issueToken(userId: string) {
+    const data = { id: userId };
+    return { access_token: await this.jwtService.signAsync(data) };
+  }
 }
