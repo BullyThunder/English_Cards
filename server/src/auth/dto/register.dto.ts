8d7b556b8edd0f50edfe9d060ff76e0060097c36
Bully_Thunder <vamosbarca2407@gmail.com>
2026-01-19 11:31:05 +0200

    fix: prisma configuration

--- server/src/auth/dto/register.dto.ts
+++ server/src/auth/dto/register.dto.ts
@@ -1,18 +1,20 @@
-import {IsEmail, IsString, MinLength, IsOptional} from 'class-validator';
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
 
 export class RegisterDto {
-    @IsEmail({
-        allow_display_name: false,
-        host_blacklist: ['spam.com']
-    }, 
-        {message: 'Incorrect email format'})
-    email: string;
+  @IsEmail(
+    {
+      allow_display_name: false,
+      host_blacklist: ['spam.com'],
+    },
+    { message: 'Incorrect email format' },
+  )
+  email: string;
 
-    @IsString()
-    @MinLength(6, { message: 'Use 6 or more characters for your password' })
-    password: string;
+  @IsString()
+  @MinLength(6, { message: 'Use 6 or more characters for your password' })
+  password: string;
 
-    @IsString()
-    @IsOptional()
-    name?: string;
+  @IsString()
+  @IsOptional()
+  name?: string;
 }
